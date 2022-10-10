8f98b4d36019f342d4481c692652196142489724
Haolat3 <92693957+Haolat3@users.noreply.github.com>
2022-10-10 10:54:47 -0400

    Create Lab0_Problem1.cpp

---
+++ Lab0/Lab0_Problem1.cpp
@@ -0,0 +1,24 @@
+/*
+Author:  Haolat Adebayo
+Date last modified: 9/12/2022
+Organization: ECE4122_6122 Class
+
+Description:
+
+Simple program to print a print my name and a few punctuation
+marks 
+    
+*/
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    cout << "My name is: Haolat, Adebayo." << endl; 
+    cout << "This ('') is a double quote." << endl;
+	cout << "This (') is a single quote." << endl;
+	cout << "This (\\) is a backslash." << endl;
+	cout << "This (/) is a forward slash." << endl;
+}    
